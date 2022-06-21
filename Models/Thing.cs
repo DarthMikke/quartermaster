363dcbb26483c27e0386fbfb5b221b128926840d
Michal Jan Warecki <mwarecki1@gmail.com>
2022-06-21 23:24:32 +0200

    Add new thing to a category.

--- Models/Thing.cs
+++ Models/Thing.cs
@@ -7,4 +7,12 @@ public class Thing
     public Amount Amount { get; set; }
     public int CategoryId { get; set; }
     public int Multiplier { get; set; }
+
+    public Thing(int CategoryId) {
+        this.Id = 0;
+        this.Make = "Ny ting";
+        this.Amount = new Amount(100, Unit.Gram);
+        this.CategoryId = CategoryId;
+        this.Multiplier = 1;
+    }
 }
